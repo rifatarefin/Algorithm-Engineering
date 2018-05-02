5df88f93ee10c0a4dfb0be407674135ebf700465
rifatarefin <rifatarefin19@gmail.com>
2018-05-02 10:22:50 +0600

    off2 2

--- Offline 2/bitmask.cpp
+++ Offline 2/bitmask.cpp
@@ -4,7 +4,7 @@ ifstream fin("in.txt");
 
 int N,M;                         //N = number of subset;M = size of universe.
 int a[1000];
-double w[1000];
+double w[1000],minC=INT_MAX;
 set<int>result;
 void input()
 {
@@ -13,6 +13,7 @@ void input()
     memset(a,0,sizeof(a[0]));
     memset(w,0,sizeof(w[0]));
     result.clear();
+    minC=INT_MAX;
     getline(fin,line);
     for(int i=0;i<N;i++)
     {
@@ -30,19 +31,25 @@ void input()
         //cout<<mask<<endl;              //print
     }
 
-
 }
 
-double subset(int mask, int idx)
+void subset(int mask, int idx, double cost, set<int>cover)
 {
-    if(idx==N && mask!=(1<<M)-1)return 99999;
-    else if(idx==N && mask==(1<<M)-1)return 0;
-    double take=subset(mask|a[idx],idx+1)+w[idx];
-    double discard=subset(mask,idx+1);
-    //cout<<w[idx]<<endl;;
-    double t= min(take,discard);
-    if(t==take)result.insert(idx);
-    return t;
+    if(idx==N && mask!=(1<<M)-1)return;
+    else if(idx==N && mask==(1<<M)-1)
+    {
+        if(cost<minC)
+        {
+            minC=cost;
+            result=cover;
+        }
+        return ;
+    }
+    set<int>nn=cover;
+    nn.insert(idx);
+    subset(mask|a[idx],idx+1,cost+w[idx],nn);
+    subset(mask,idx+1,cost,cover);
+
 }
 
 int main()
@@ -52,10 +59,11 @@ int main()
     for(int xx=0;xx<tc;xx++)
     {
         input();
-        double setsum= subset(0,0);
-        cout<<setsum<<endl;
+        set<int>sd;
+        subset(0,0,0,sd);
+        cout<<"Cost="<<minC<<", Subsets: ";
         for(set<int>::iterator it=result.begin();it!=result.end();it++)cout<<*it<<" ";
-        cout<<"\n";
+        cout<<"\n\n";
 
     }
 }
